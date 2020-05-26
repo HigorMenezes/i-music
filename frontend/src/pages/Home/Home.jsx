6b5091ef145a11f807f783246fa251bb85c60731
Higor Menezes <higormenezes1996@gmail.com>
2020-05-26 15:48:33 -0300

    create axios instance

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
+import axios from '../../core/axios';
 
 import { getCookie } from '../../utils/cookie';
 
 function Home() {
   useEffect(() => {
     axios
-      .get('https://api.spotify.com/v1/me', {
+      .get('/me', {
         headers: { Authorization: `Bearer ${getCookie('access_token')}` },
       })
       .then(({ data }) => {
